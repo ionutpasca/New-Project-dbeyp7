54841df97151b321643d19b7b001d467910a1677
Pașca Ionuț <pascaionut94@gmail.com>
2022-01-14 16:54:37 +0200

    test commit

--- src/store.tsx
+++ src/store.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 const AppStateContext = createContext<Props | null>(null);
 
+// comment
 export const AppStateProvider: React.FC<Props> = ({ children }) => {
   const [state, setState] = useState<State>({
     title: "teleport-project-react"
